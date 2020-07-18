fe2e4843f179d6ecc5c138f3b4208517174cd27a
Mario <mario.nascimento@aluno.uece.br>
2020-07-17 21:41:43 -0300

    add profile photo to user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User as DjangoUser
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to='profiles')
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    def __str__(self):
+        return self.username
